ee60a79b0af582b3316814426e182256d4582551
Dian <dian7875.a@gmail.com>
2024-11-10 20:45:51 -0600

    Fix

--- src/Auth/Hooks/UseLogin.tsx
+++ src/Auth/Hooks/UseLogin.tsx
@@ -5,12 +5,12 @@ import { LogIn } from "../Services/Auth";
 import { useLocation } from "react-router";
 
 const UseLogin = () => {
-  const baseURL = import.meta.env.VITE_SECONDARY_URL
+  const secondaryURL = import.meta.env.VITE_SECONDARY_URL
 
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const redirectUrl =
-    params.get("redirect") || "";
+    params.get("redirect") || secondaryURL;
   return useMutation({
     mutationFn: (data: credencial) =>
       toast.promise(LogIn(data), {
